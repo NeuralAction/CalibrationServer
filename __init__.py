b3f1d750e512d41cbd1f8678c8a72d3e20431357
kidevelop <hjh010501@naver.com>
2020-02-25 18:01:57 +0900

    (add): Test api

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ UPLOAD_FOLDER = 'data'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods=['POST'])
-def upload_train():
+def upload_data():
     uploaded_files = request.files.getlist("file")
     for file in uploaded_files:
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
             print(file)
     return {}, 200
 
-app.run(port=5000, host='0.0.0.0', threaded=True)
+@app.route('/test', methods=['POST'])
+def test():
+    return {"message": "Server connection test complete! Go to calibrate work."}, 200
+
+app.run(port=5000, host='0.0.0.0', debug=True, threaded=True) # 192.168.0.37
